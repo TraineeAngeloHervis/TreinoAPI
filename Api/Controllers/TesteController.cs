1826d2d4cf46f3be029953a83ce801e9f093bd01
Ângelo Adir Hervis <angelo.hervis@gmail.com>
2024-05-23 16:51:26 -0300

    Estrutura Projeto API Swagger

---
+++ Api/Controllers/TesteController.cs
@@ -0,0 +1,21 @@
+﻿using Crosscutting;
+using Domain;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TesteController : ControllerBase
+    {
+        [HttpGet("teste")]
+        public IActionResult Testar(
+            [FromQuery] TesteRequisicaoDto requisicao,
+            [FromServices] ITesteService service)
+        {
+            var resposta = service.TesteMetodo(requisicao);
+
+            return Ok(resposta);
+        }
+    }
+}
---
+++ Domain/CombinadorDeStringService.cs
@@ -0,0 +1,10 @@
+﻿namespace Domain
+{
+    public class CombinadorDeStringService : ICombinadorDeStringService
+    {
+        public string CombinarPartes(IEnumerable<string> partes) 
+        { 
+            return string.Join(",", partes);
+        }
+    }
+}
